9c9afab9fec323bf00adaa0ee92b06fc9f7f6d2c
Doug Winegarden <d3wine@gmail.com>
2020-05-01 16:19:05 -0700

    Initial Grid Based Inv. scripts

---
+++ Assets/Scripts/ItemMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMovement : MonoBehaviour
+{
+    public Camera camera;
+    public GameObject selectedItem = null;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform objectHit = hit.transform;
+                selectedItem = objectHit.parent.gameObject;
+
+                // Do something with the object that was hit by the raycast.
+            }
+        }
+    }
+}
