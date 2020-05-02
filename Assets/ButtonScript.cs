3f2428a1a5500c72cf5524a942a3c4294583b1e3
Doug Winegarden <d3wine@gmail.com>
2020-05-01 23:47:42 -0700

    Added intro scene

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        //Load the game... how to do tutorial??
+        SceneManager.LoadScene("Office1");
+        gameObject.SetActive(false);
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
