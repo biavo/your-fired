4ae7ba9368d19e386133f2cba0b784a6746a9e0d
Doug Winegarden <d3wine@gmail.com>
2020-05-02 01:09:58 -0700

    Added new Items
    also, cleaned up MouseLook

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -8,16 +8,6 @@ public class MouseLook : MonoBehaviour
     float mouseSensitivity = 100f;
     public Transform playerBody;
     float xRotation = 0f;
-    //public int stapler;
-    //public int printer;
-    //public int pen;
-    //public int fancyPen;
-    //public int mug;
-    //public int monitor;
-    //public int mouse;
-    //public int keyboard;
-    //public int computer;
-    //public int laptop;
 
     public Transform ItemParent;
 
@@ -28,6 +18,24 @@ public class MouseLook : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+    }
+
+    void spawnItem(RaycastHit hit)
+    {
+        Vector3 spawnPos;
+        var a = Random.Range(1, 3);
+        //Debug.Log(a);
+        if (a == 1)
+        {
+            spawnPos = new Vector3(spawnPos1.position.x + Random.Range(-3.5f, 3.5f), spawnPos1.position.y, spawnPos1.position.z + Random.Range(-1.5f, 1.25f));
+        }
+        else
+        {
+            spawnPos = new Vector3(spawnPos2.position.x + Random.Range(-3.5f, 3.5f), spawnPos2.position.y, spawnPos2.position.z + Random.Range(-1.25f, 1.5f));
+        }
+        //Vector3 spawnPos = ItemParent.position;
+        var o = Instantiate(Resources.Load<GameObject>(hit.transform.gameObject.tag + "Inv"), spawnPos, Quaternion.identity, ItemParent);
+        o.transform.localEulerAngles = new Vector3(0, 0, 0);
     }
        
     void Update() {
@@ -43,70 +51,10 @@ public class MouseLook : MonoBehaviour
                 if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Item"))
                 {
                     Destroy(hit.transform.gameObject);
-                    Vector3 spawnPos;
-                    var a = Random.Range(1, 3);
-                    //Debug.Log(a);
-                    if(a == 1)
-                    {
-                        spawnPos = new Vector3(spawnPos1.position.x + Random.Range(-3.5f, 3.5f), spawnPos1.position.y, spawnPos1.position.z + Random.Range(-1.5f, 1.5f));
-                    }
-                    else
-                    {
-                        spawnPos = new Vector3(spawnPos2.position.x + Random.Range(-3.5f, 3.5f), spawnPos2.position.y, spawnPos2.position.z + Random.Range(-1.5f, 1.5f));
-                    }
-                    //Vector3 spawnPos = ItemParent.position;
-                    var o = Instantiate(Resources.Load<GameObject>(hit.transform.gameObject.tag + "Inv"), spawnPos, Quaternion.identity, ItemParent);
-                    o.transform.localEulerAngles = new Vector3(0, 0, 0);
+                    spawnItem(hit);
                 }
             }
         }
-
-        //if(Physics.Raycast( Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 5f)){
-        //    print("hit");
-        //    if(Input.GetButtonDown("Fire1")){
-        //        print("fire");
-        //        if(hit.transform.gameObject.tag == ("Pen")){
-        //            pen ++;
-        //            Destroy(hit.transform.gameObject);
-        //        }
-        //        if(hit.transform.gameObject.tag == ("Printer")){
-        //            printer ++;
-        //            Destroy(hit.transform.gameObject);
-        //        }
-        //        if(hit.transform.gameObject.tag == ("Stapler")){
-        //            stapler ++;
-        //            Destroy(hit.transform.gameObject);
-        //        }
-        //        if(hit.transform.gameObject.tag == ("FancyPen")){
-        //            fancyPen ++;
-        //            Destroy(hit.transform.gameObject);
-        //        }
-        //        if(hit.transform.gameObject.tag == ("Mug")){
-        //            mug ++;
-        //            Destroy(hit.transform.gameObject);
-        //        }
-        //        if(hit.transform.gameObject.tag == ("Monitor")){
-        //            monitor ++;
-        //            Destroy(hit.transform.gameObject);
-        //        }
-        //        if(hit.transform.gameObject.tag == ("Mouse")){
-        //            mouse ++;
-        //            Destroy(hit.transform.gameObject);
-        //        }
-        //        if(hit.transform.gameObject.tag == ("Keyboard")){
-        //            keyboard ++;
-        //            Destroy(hit.transform.gameObject);
-        //        }
-        //        if(hit.transform.gameObject.tag == ("Computer")){
-        //            computer ++;
-        //            Destroy(hit.transform.gameObject);
-        //        }
-        //        if(hit.transform.gameObject.tag == ("Laptop")){
-        //            computer ++;
-        //            Destroy(hit.transform.gameObject);
-        //        }
-        //    }
-        //}
 
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
